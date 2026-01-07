ba19d968e8fd7e24db7992f0fe08e5d49551b9ba
Kapi <kacperkrzyzewski20@gmail.com>
2026-01-07 15:56:37 +0100

    Fixed another mistakes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from states.base_state import State
 from states.main_menu_state import MainMenuState
+from states.gameplay_states import GameState
 import pygame
 import sys
 import ctypes
@@ -17,14 +19,14 @@ class AngryKnightsApp:
         self.clock = pygame.time.Clock()
         self.running = True
 
-        self.state = MainMenuState(self._screen_size)
+        self._state = MainMenuState(self._screen_size)
 
-    def screen_size(self):
+    def screen_size(self) -> pygame.Vector2:
         screen_x = self._screen.get_width()
         screen_y = self._screen.get_height()
         return pygame.Vector2(screen_x, screen_y)
 
-    def _change_screen_mode(self):
+    def _change_screen_mode(self) -> None:
         if self._is_fullscreen:
             self._screen = pygame.display.set_mode((1920, 1080), pygame.SCALED)
             self._is_fullscreen = False
@@ -34,16 +36,17 @@ class AngryKnightsApp:
                                                    pygame.SCALED)
             self._is_fullscreen = True
 
-    def run(self):
+    def run(self) -> None:
         delta_time = 0.1
         while self.running:
             events = pygame.event.get()
             # 1. Przekaż obsługę logiki do aktualnego stanu
             # Stan zwraca samego siebie lub NOWY stan
-            self.state = self.state.update(events)
+            result = self._state.update(events)
+            self._state = self._manage_states(result)
 
             # 2. Rysowanie
-            self.state.draw(self._screen)
+            self._state.draw(self._screen)
 
             for event in events:
                 if event.type == pygame.QUIT:
@@ -54,6 +57,15 @@ class AngryKnightsApp:
             pygame.display.flip()
             delta_time = self.clock.tick(60) / 1000
             delta_time = max(0.001, min(0.1, delta_time))
+
+    def _manage_states(self, result: State | str) -> State:
+        if isinstance(result, State):
+            return result
+        elif result == "GO_TO_MENU":
+            self.state = MainMenuState(self._screen_size)
+        elif result == "START_GAME":
+            self.state = GameState(self._screen_size, 1)
+        return self.state
 
 
 if __name__ == '__main__':
